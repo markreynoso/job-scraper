488609cf4cc4a2b26da065206ef42a51f87afe05
Mark Reynoso <mreynoso@spu.edu>
2018-05-16 15:02:54 -0700

    testing alternative site

--- scraper.py
+++ scraper.py
@@ -3,25 +3,35 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 
 
-base_url = 'https://stackoverflow.com/jobs?med=site-ui&ref=jobs-tab'
+# base_url = 'https://stackoverflow.com/jobs?med=site-ui&ref=jobs-tab'
+
+# def job_scraper(base_url):
+#     url_list = []
+#     for x in range(10):
+#         x += 1
+#         url_list.append(base_url + '&pg={}'.format(x))
+#     for url in url_list:
+#         # page = requests.get(url, verify=False)
+#         page = urlopen(url)
+#         soup = bs(page)
+#         tags = soup.find_all('a', class_='post-tag job-link no-tag-menu')
+#         import pdb; pdb.set_trace()
+#         # for tag in tags:
+
+
+# job_scraper(base_url)
+
+base_url = 'http://www.subsplash.com/careers/'
 
 def job_scraper(base_url):
-    url_list = []
-    for x in range(10):
-        x += 1
-        url_list.append(base_url + '&pg={}'.format(x))
-    for url in url_list:
         # page = requests.get(url, verify=False)
-        page = urlopen(url)
+        page = urlopen(base_url)
         soup = bs(page)
-        tags = soup.find_all('a', class_='post-tag job-link no-tag-menu')
+        tags = soup.find_all('li', class_='BambooHR-ATS-Department-Header')
         import pdb; pdb.set_trace()
         # for tag in tags:
 
-
 job_scraper(base_url)
-
-
 
 # - The job title
 # - The company name 
